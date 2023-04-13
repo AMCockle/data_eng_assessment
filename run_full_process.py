d1fcc343dab3cf08d227cf4f75428d33a02a2450
AMCockle <anna_cockle@hotmail.com>
2023-04-13 20:01:54 +0100

    missing file path

--- run_full_process.py
+++ run_full_process.py
@@ -2,4 +2,4 @@ import connect_postgres
 import extract_patient_data
 
 connect_postgres.prepare_data_tables()
-extract_patient_data.insert_patient_json_data()
+extract_patient_data.insert_patient_json_data("data")
